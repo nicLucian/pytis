752932b0f889fc14e71f026d70e706c25dedc63d
Milan Zamazal <pdm@brailcom.org>
2011-05-06 14:45:04 +0200

    Ensure full ListForm initialization before accessing its table in can_command

--- lib/pytis/form/list.py
+++ lib/pytis/form/list.py
@@ -1514,6 +1514,8 @@ class ListForm(RecordForm, TitledForm, Refreshable):
                          ListForm.COMMAND_FINISH_EDITING,
                          ListForm.COMMAND_CELL_COMMIT,
                          ListForm.COMMAND_CELL_ROLLBACK)
+        if not self.initialized():
+            self.full_init()
         if self._table.editing():
             allowed = EDIT_COMMANDS
             if not self._grid.IsCellEditControlEnabled():
